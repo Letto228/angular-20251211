a8ee1dde00cc4a171bc02b4ae627aa801d229c29
Abbazova Regina <missregina93@gmail.com>
2025-12-21 22:28:05 +0300

    feat: create product-card component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {Header} from './main-components/header/header';
+import {ProductCard} from './pages/products-list/card/product-card';
 
 @Component({
     selector: 'app-root',
-    imports: [Header],
+    imports: [Header, ProductCard],
     templateUrl: './app.html',
     styleUrl: './app.css',
 })
