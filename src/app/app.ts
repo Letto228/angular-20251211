9ee30a3d10568006e6efd1be5a3900c257a692a6
Дарья Иконникова <ikonnikova.darya2@wb.ru>
2025-12-24 17:56:58 +0300

    feat(): create card component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {Header} from './main-components/header/header';
+import {Card} from './pages/products-list/card/card';
 
 @Component({
     selector: 'app-root',
-    imports: [Header],
+    imports: [Header, Card],
     templateUrl: './app.html',
     styleUrl: './app.css',
 })
