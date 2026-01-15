1c87ce646b1a6674ee91184033834b294183ccfb
LearnJS <learnjs@MacBook-Pro-Egor.local>
2026-01-15 23:25:37 +0300

    feat: add homework stand

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {Header} from './main-components/header/header';
 import {Sidenav} from './main-components/sidenav/sidenav';
 import {MatList, MatListItem} from '@angular/material/list';
@@ -14,4 +14,19 @@ import {ProductsList} from './pages/products-list/products-list';
 export class App {
     protected readonly title = 'angular-20251211';
     protected readonly imgSrc = './favicon.ico';
+
+    readonly switchTemplate = signal(false);
+    readonly closeTemplate = signal(true);
+
+    constructor() {
+        setInterval(() => {
+            this.toggleTemplate();
+        }, 3000);
+    }
+
+    private toggleTemplate() {
+        this.switchTemplate.set(!this.switchTemplate());
+        // or
+        this.closeTemplate.set(!this.closeTemplate());
+    }
 }
