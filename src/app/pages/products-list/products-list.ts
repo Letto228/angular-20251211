20611177d494a6a57b63e1b0527a89b059efdf4d
LearnJS <learnjs@MacBook-Pro-Egor.local>
2026-01-19 23:45:51 +0300

    chore: sanitize project

--- src/app/pages/products-list/products-list.ts
+++ src/app/pages/products-list/products-list.ts
@@ -4,12 +4,6 @@ import {productsMock} from '../../shared/products/products.mock';
 import {Product} from '../../shared/products/product.type';
 import {NgFor, NgIf} from '@angular/common';
 
-// type CardTemplateContext = {
-//     currentItem: Product;
-//     index: number;
-//     $implicit: Product;
-// }
-
 @Component({
     selector: 'app-products-list',
     imports: [Card, NgFor, NgIf],
@@ -21,37 +15,12 @@ export class ProductsList {
     readonly products = signal<null | Product[]>(null);
 
     constructor() {
-        // setTimeout(() => {
-        //     this.products.update(products => ([...products]).reverse())
-        // }, 3000);
-
         setTimeout(() => {
             this.products.set(productsMock);
         }, 3000);
-
-        setTimeout(() => {
-            this.products.set(productsMock.map(product => ({...product})));
-            // this.products.set([...productsMock]);
-        }, 6000);
     }
 
     productTrackBy(index: number, item: Product) {
         return item._id;
     }
-
-    // private readonly cardTemplateRef = viewChild.required<TemplateRef<CardTemplateContext>>('cardTemplate');
-    // private readonly cardViewContainerRef = viewChild.required('viewPort', {
-    //     read: ViewContainerRef,
-    // });
-
-    // constructor() {
-    //     setTimeout(() => {
-    //         this.products().forEach((product, index) => {
-    //             this.cardViewContainerRef().createEmbeddedView(
-    //                 this.cardTemplateRef(),
-    //                 {currentItem: product, index, $implicit: product}
-    //             );
-    //         });
-    //     }, 3000);
-    // }
 }
