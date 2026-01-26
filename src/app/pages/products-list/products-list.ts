092fd6d7395b66e5c52af7907eb7280e327e0318
LearnJS <learnjs@MacBook-Pro-Egor.local>
2026-01-26 23:50:50 +0300

    feat: add scroll with loading

--- src/app/pages/products-list/products-list.ts
+++ src/app/pages/products-list/products-list.ts
@@ -3,10 +3,11 @@ import {Card} from './card/card';
 import {productsMock} from '../../shared/products/products.mock';
 import {Product} from '../../shared/products/product.type';
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {ScrollWithLoadingDirective} from '../../shared/scroll-with-loading/scroll-with-loading.directive';
 
 @Component({
     selector: 'app-products-list',
-    imports: [Card, MatProgressSpinner],
+    imports: [Card, MatProgressSpinner, ScrollWithLoadingDirective],
     templateUrl: './products-list.html',
     styleUrl: './products-list.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,7 +21,7 @@ export class ProductsList {
         }, 3000);
     }
 
-    productTrackBy(index: number, item: Product) {
-        return item._id;
+    protected loadNextData() {
+        console.log('Load next data');
     }
 }
