3526073b912163db8257a178153c12be62448940
LearnJS <learnjs@MacBook-Pro-Egor.local>
2026-02-02 23:13:06 +0300

    chore: sanitize project

--- src/app/shared/products/products-api.service.ts
+++ src/app/shared/products/products-api.service.ts
@@ -8,26 +8,9 @@ export class ProductsApiService {
     private readonly httpClient = inject(HttpClient);
 
     getProducts$(): Observable<Product[]> {
-        // return of([])
-        // return of(productsMock);
-
-        return (
-            this.httpClient
-                // .get<{data: {items: Product[]}}>(`${baseUrl}/products/suggestion`)
-                .get<{data: {items: Product[]}}>(`/products/suggestion`)
-                .pipe(
-                    map(productsDto => productsDto.data.items),
-                    // retry({
-                    //     count: 3,
-                    //     delay: 3000,
-                    // }),
-                    // tap({
-                    //     error: () => {
-
-                    //     }
-                    // }),
-                    catchError(() => of([])),
-                )
+        return this.httpClient.get<{data: {items: Product[]}}>(`/products/suggestion`).pipe(
+            map(productsDto => productsDto.data.items),
+            catchError(() => of([])),
         );
     }
 }
