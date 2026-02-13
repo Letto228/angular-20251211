09498a7e1426ddc379005456235e71b4b9a35976
LearnJS <learnjs@MacBook-Pro-Egor.local>
2026-02-13 09:58:39 +0300

    chore: sanitize project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import {Routes} from '@angular/router';
 import {NotFound} from './pages/not-found/not-found';
-import {canDeactivateTestGuard} from './shared/test-guards/can-deactivate-test.guard';
-import {canMatchTestGuard} from './shared/test-guards/can-match-test.guard';
 
 export const routes: Routes = [
     {
@@ -13,15 +11,11 @@ export const routes: Routes = [
         path: 'products-list',
         loadComponent: () =>
             import('./pages/products-list/products-list').then(m => m.ProductsList),
-        canDeactivate: [canDeactivateTestGuard],
     },
     {
         path: 'product/:id',
         loadComponent: () => import('./pages/product/product').then(m => m.Product),
         loadChildren: () => import('./pages/product/product.routes').then(m => m.productRoutes),
-        // canActivate: [canActivateTestGuard],
-        // canActivateChild: [canActivateChildTestGuard],
-        canMatch: [canMatchTestGuard],
     },
     {
         path: '**',
--- src/app/pages/products-list/products-list.ts
+++ src/app/pages/products-list/products-list.ts
@@ -5,9 +5,7 @@ import {MatProgressSpinner} from '@angular/material/progress-spinner';
 import {ScrollWithLoadingDirective} from '../../shared/scroll-with-loading/scroll-with-loading.directive';
 import {FilterByPropertyPipe} from '../../shared/filter-by-property/filter-by-property-pipe';
 import {ProductsStoreService} from '../../shared/products/products-store.service';
-import {Router, RouterLink} from '@angular/router';
-import {Counter} from '../../shared/counter/counter';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
 import {JsonPipe} from '@angular/common';
 import {IsStringValidator} from './validators/is-string-validator/is-string-validator';
@@ -20,9 +18,6 @@ import {IsStringValidator} from './validators/is-string-validator/is-string-vali
         ScrollWithLoadingDirective,
         FilterByPropertyPipe,
         RouterLink,
-        Counter,
-        ReactiveFormsModule,
-        FormsModule,
         MatInput,
         MatFormField,
         MatLabel,
@@ -35,45 +30,9 @@ import {IsStringValidator} from './validators/is-string-validator/is-string-vali
 })
 export class ProductsList {
     private readonly productsStoreService = inject(ProductsStoreService);
-    private readonly router = inject(Router);
-
-    // counterStore = signal(22);
-
-    // protected readonly counterControl = new FormControl(0);
-
-    // counterControlValue = toSignal(this.counterControl.valueChanges);
-
-    // filteredValue = computed(() => {
-    //     const array = [];
-
-    //     return array.filter(item => item === this.counterControlValue());
-    // })
-
-    // ----
-
-    // protected readonly searchControl = new FormControl('', {
-    //     validators: [isStringValidator, Validators.required, Validators.minLength(3)],
-    // });
 
     constructor() {
         this.productsStoreService.loadProducts();
-
-        // this.counterControl.invalid
-        // this.counterControl.valid
-        // this.counterControl.touched
-        // this.counterControl.untouched
-        // this.counterControl.pristine
-        // this.counterControl.dirty
-        // this.counterControl.value;
-        // this.counterControl.setValue(12);
-
-        // setTimeout(() => {
-        //     // this.counterControl.setValue(22);
-        //     // this.counterStore.set(34);
-        // }, 3000);
-
-        // this.counterControl.valueChanges.subscribe(console.log);
-        // toSignal(this.counterControl.valueChanges);
     }
 
     protected products(): Product[] | null {
